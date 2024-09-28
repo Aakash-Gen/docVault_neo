7f3fc540c0391330dbf1213396121e79afa078b2
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2024-09-28 20:37:14 +0530

    Shad CN setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
 		"./index.html",
 		"./src/**/*.{js,ts,jsx,tsx}",
 	],
   theme: {
-    extend: {},
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+				primaryColor: '#007ACC',
+				primaryBackground: 'gray-900',
+  			background: 'hsl(var(--background))',
+				primaryBlack: '#0D111D',
+				primaryGreen: '#27E8A7',
+				primaryGray: '#3E3E42',
+			}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 
